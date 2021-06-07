a7de674db43f1482c49cef124b6cff6168133dad
Danish_Ahmed <qureshidanish78@gmail.com>
2021-06-07 12:43:59 +0500

    add categories crud

--- client/src/components/categories/CategoriesList.js
+++ client/src/components/categories/CategoriesList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   List,
   Datagrid,
@@ -10,21 +10,21 @@ import {
 } from "react-admin";
 import authProvider from "../../authProvider";
 
-const ShopFilter = (props) => (
-  <Filter {...props}>
-    <TextInput label="Search Shop Name" source="shopname" />
-    <TextInput label="Commercial Id" source="CommercialID" defaultValue="" />
-  </Filter>
-);
-
-const CategoriesList =  async (props) => {
-  const [permission, getPermissions] = useState("");
+const CategoriesList = (props) => {
+  const [permission, setPermissions] = useState("");
+  useEffect(() => {
+    authProvider
+      .getPermissions()
+      .then((permission) => setPermissions(permission));
+  }, "none");
   return (
     <List {...props}>
       <Datagrid>
         <TextField source="_id" />
         <TextField source="name" />
-        {/* {permission == "admin" && <EditButton basePath="/shops" label="" />} */}
+        {permission == "admin" && (
+          <EditButton basePath="/categories" label="Edit" />
+        )}
       </Datagrid>
     </List>
   );
